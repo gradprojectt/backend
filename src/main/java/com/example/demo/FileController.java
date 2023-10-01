e11374b6587ce9ba02b35c534e642094e222b76e
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2023-10-02 00:37:23 +0900

    image api

---
+++ src/main/java/com/example/demo/FileController.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.Context;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+public class FileController {
+    private final FileService fileService;
+    public FileController(FileService fileService) {
+        this.fileService = fileService; // Assuming FileService is a service that handles files
+    }
+
+    @PostMapping(value = "/upload")
+    public ResponseEntity<?> uploadFile(MultipartFile file) {
+        if(file.isEmpty()) {
+            /* 파일을 업로드 하지 않았을 경우 처리 */
+        }
+
+        String fileName = file.getOriginalFilename(); // Assuming you want to use the original filename
+
+        String downloadURI = ServletUriComponentsBuilder.fromCurrentContextPath()
+                                                        .path("/download/")
+                                                        .path(fileName)
+                                                        .toUriString();
+
+        return new ResponseEntity<>(downloadURI, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/download")
+    public ResponseEntity<?> downloadFile(@RequestParam("fileName") String fileName, HttpServletRequest request) throws FileNotFoundException {
+
+        Resource resource = fileService.loadFile(fileName);
+
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            log.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if(contentType == null) {
+            contentType = "application/octet-stream";
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+}
+
+
--- src/main/java/com/example/demo/PersonalcolorApplication.java
+++ src/main/java/com/example/demo/PersonalcolorApplication.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(
+		{FileUploadProperties.class}
+)
 @SpringBootApplication
 public class PersonalcolorApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PersonalcolorApplication.class, args);
 	}
 
