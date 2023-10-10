cbe8d4353a8fc9b4e7778b323cdeba15d0016595
seyeonyy <123892130+seyeonyy@users.noreply.github.com>
2023-10-11 01:59:31 +0900

    testcode deleted

--- src/test/java/com/example/demo/PersonalcolorApplicationTests.java
+++ src/test/java/com/example/demo/PersonalcolorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+/*package com.example.demo;
 
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
@@ -20,4 +20,4 @@ class PersonalcolorApplicationTests {
 //	void contextLoads() {
 //	}
 
-}
+}*/
